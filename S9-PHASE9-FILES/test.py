1e30a4b60cc5f919214ed960e92831b1a26005b6
megh-malvankar <megh.malvankar@gmail.com>
2026-08-29 00:45:31 +0530

    Add files via upload

---
+++ S9-PHASE9-FILES/test.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy import stats
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_absolute_error, r2_score
+import joblib
+
+print("========================================")
+print(" All Required Libraries Installed ")
+print("========================================")
